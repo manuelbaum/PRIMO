9e9ecb4fb4cd1a770d9971ed9387de41afc8947d
Lukas Kettenbach <lkettenb@TechFak.Uni-Bielefeld.DE>
2012-11-12 12:21:13 +0100

    Removed src dir

--- src/ExampleBayesNet.py
+++
@@ -1,8 +0,0 @@
-from  core.BayesNet import *
-from  core.Node import *
-
-bn = BayesNet()
-n = Node()
-
-bn.add_node(n)
-bn.add_node("lol")
--- src/core/Node.py
+++
@@ -1,2 +0,0 @@
-class Node(object):
-    name = "SomeNode"
