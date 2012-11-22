545c00538108a5ec68659b0df62847abdf9e3340
mbaumBielefeld <mbaum@TechFak.Uni-Bielefeld.DE>
2012-11-22 15:05:48 +0100

    CPT added

--- reasoning/CPT.py
+++ reasoning/CPT.py
@@ -1,10 +1,22 @@
 # -*- coding: utf-8 -*-
 
 from reasoning.AbstractCPD import AbstractCPD
-
+from numpy import array
 
 class CPT(AbstractCPD):
     '''TODO: write doc'''
 
+    self.values = numpy.array(0)
+
     def __init__(self):
         super(CPT, self).__init__()
+
+    def multiplication(self, factor):
+        
+    def marginalization()
+
+    def reduction()
+
+    def division()
+
+
