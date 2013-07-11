fixed some bug in computation of acceptance probability in MetropolisHastings, now works with restrained value ranges too. Also changed sample_local in continuous node to direct sampling so there is no rejection sampling anymore

primo/reasoning/ContinuousNode.py
@@ -2,6 +2,7 @@
 
 from primo.reasoning import RandomNode
 import random
+import scipy
 
 class ContinuousNode(RandomNode):
     '''
@@ -53,18 +54,44 @@ class ContinuousNode(RandomNode):
         reimplement it by constructing the integral over the normalvariate in the
         intervalls allowed by the evidence and then generate a sample directly.
         Currently this method has O(inf).'''
-        std_walk=0.1
-        v=random.normalvariate(x,std_walk)
-        if self in evidence.keys():
-            while not (evidence[self].is_compatible(v) and self.value_range[0]<v and v<self.value_range[1]):
-                v=random.normalvariate(x,std_walk)
-        else:
-            while not (self.value_range[0]<v and v<self.value_range[1]):
-                v=random.normalvariate(x,std_walk)
-        #print "REEEEEEEEEEEEEEEEEEEEEEEEEEEEEE"+self.name+" " +str(v)
-        return v
+        std_walk=1.0
         
-    def sample_global(self, state):
+        #intersect possible evidence-interval with value_range:
+        if self in evidence.keys():
+            evidence_range=evidence[self].get_interval()
+            lower_limit=max(self.value_range[0],evidence_range[0])
+            upper_limit=min(self.value_range[1],evidence_range[1])
+        else:
+            lower_limit=self.value_range[0]
+            upper_limit=self.value_range[1]
+        
+        if lower_limit==upper_limit:
+            v=lower_limit
+        if lower_limit>upper_limit:
+            raise Exception("Intersection of random variable's value_range and"
+                "allowed Interval for Evidence is empty - no sampling possible")
+        
+        #v=random.normalvariate(x,std_walk)
+        #while not (lower_limit<=v and v<=upper_limit):
+        #    v=random.normalvariate(x,std_walk)
+        
+        #generate the actual sample
+        distribution=scipy.stats.norm(x, std_walk)
+        lower_cdf=distribution.cdf(lower_limit)
+        upper_cdf=distribution.cdf(upper_limit)
+        
+        sample_in_integral=random.uniform(lower_cdf, upper_cdf)
+        
+        sample=distribution.ppf(sample_in_integral)
+        
+        
+        
+        a=scipy.stats.norm(self.value_range[0], std_walk).cdf(x)
+        b=scipy.stats.norm(self.value_range[0], std_walk).cdf(sample)
+        cdf_ratio = a/b 
+        return sample,cdf_ratio
+        
+    def sample_global(self, state, evidence):
         '''
         This method can be used to sample from this local distribution.
         
@@ -72,7 +99,31 @@ class ContinuousNode(RandomNode):
             values of this nodes parents in this dict and the conditional 
             probability density will be adjusted accordingly.
         '''
-        proposal=self.cpd.sample_global(state)
+        #is there some evidence for this node?
+        if self in evidence.keys():
+            #if only one value is allowed we can return it immediatly
+            unique=evidence[self].get_unique_value()
+            if unique!=None:
+                return unique
+            #if a whole interval is allowed intersect it with this variable's
+            #value_range to get limits for sampling
+            else:
+                evidence_range=evidence[self].get_interval()
+                lower_limit=max(self.value_range[0],evidence_range[0])
+                upper_limit=min(self.value_range[1],evidence_range[1])
+        #without evidence this variable's value_range represents limits for sampling
+        else:
+            lower_limit=self.value_range[0]
+            upper_limit=self.value_range[1]
+        #check if only one value is allowed and in case return immediatly
+        if lower_limit==upper_limit:
+            return lower_limit
+        #check for empty interval
+        if lower_limit>upper_limit:
+            raise Exception("Intersection of random variable's value_range and"
+                "allowed Interval for Evidence is empty - no sampling possible")
+        
+        proposal=self.cpd.sample_global(state,lower_limit,upper_limit)
         return proposal
         
     def get_probability(self, value, state):

primo/reasoning/density/LinearExponential.py
@@ -37,7 +37,6 @@ class LinearExponential(Density):
         
         #Compute the offset for the density and displace the value accordingly
         _lambda = self._compute_lambda_given_parents(dict(node_value_pairs))
-        #print "lambda:"+str(_lambda)
         #Evaluate the displaced density at value
         return _lambda*math.exp(-_lambda*value)
 
@@ -49,8 +48,17 @@ class LinearExponential(Density):
         _lambda=self.output_scale*1.0/(1.0+math.exp(-x*self.input_scale))
         return _lambda
 
-    def sample_global(self,state):
+    def sample_global(self,state, lower_limit, upper_limit):
         _lambda=self._compute_lambda_given_parents(state)
-        sample=random.expovariate(_lambda)
+        distribution=scipy.stats.expon(loc=0,scale=1.0/_lambda)
+        
+        lower_cdf=distribution.cdf(lower_limit)
+        upper_cdf=distribution.cdf(upper_limit)
+        
+        sample_in_integral=random.uniform(lower_cdf, upper_cdf)
+        
+        sample=distribution.ppf(sample_in_integral)
+        
+        #sample=random.expovariate(_lambda)
         #print "EXPO-SAMPLE: "+str(sample)+" at lambda: "+str(_lambda)
         return sample

primo/reasoning/density/LinearGauss.py
@@ -65,6 +65,17 @@ class LinearGauss(Density):
                 x = x + self.b[node]*state[node]
         return x
         
-    def sample_global(self,state):
-        return random.normalvariate(self._compute_offset_given_parents(state),self.var**0.5)
+    def sample_global(self,state,lower_limit,upper_limit):
+        
+        distribution=scipy.stats.norm(self._compute_offset_given_parents(state), self.var**0.5)
+        
+        lower_cdf=distribution.cdf(lower_limit)
+        upper_cdf=distribution.cdf(upper_limit)
+        
+        sample_in_integral=random.uniform(lower_cdf, upper_cdf)
+        
+        sample=distribution.ppf(sample_in_integral)
+    
+    
+        return sample
             