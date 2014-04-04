d52cc730533b87b26880ad2af5e74e42f1e73038
Hendrik Buschmeier <hbuschme@uni-bielefeld.de>
2014-04-05 00:40:39 +0200

    Refactoring: Moved individual sampling modules to MCMC.py.

--- primo/reasoning/DiscreteDistributionSampler.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-def weighted_random(self, weights):
-    import random
-    counter = random.random() * sum(weights)
-    for i, w in enumerate(weights):
-        counter -= w
-        if counter <= 0:
-            return i
